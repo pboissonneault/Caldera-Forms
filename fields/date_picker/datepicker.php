f557c7b97e24ed35ebf7454e0864bd6f41be3614
Philippe Boissonneault <p.boissonneault@gmail.com>
2016-02-23 14:40:46 -0500

    * Fix language support for cf-datepicker

--- fields/date_picker/datepicker.php
+++ fields/date_picker/datepicker.php
@@ -13,10 +13,23 @@ if( !empty( $field['config']['end_date'] ) ){
 	$start_end_atts .= ' data-date-end-date="' . esc_attr( $field['config']['end_date'] ) . '"';
 }
 
+//PBPB : patch
+$daysOfWeekDisabled = '';
+if( !empty( $field['config']['custom_class'] ) ){
+	switch($field['config']['custom_class']) {
+		case 'samedi-seulement':
+			$daysOfWeekDisabled = 'data-date-days-of-week-disabled="0,1,2,3,4,5"';
+			break;
+		case 'dimanche-seulement':
+			$daysOfWeekDisabled = 'data-date-days-of-week-disabled="1,2,3,4,5,6"';
+			break;
+	}
+}
+
 if( !empty( $field['config']['language'] ) ){
 	if( file_exists( CFCORE_PATH . 'fields/date_picker/js/locales/bootstrap-datepicker.' . $field['config']['language'] . '.js' ) ){
 		$has_lang = 'data-date-language="' . $field['config']['language'] . '"';
-		wp_enqueue_script( 'cf-frontend-date-picker-lang', CFCORE_URL . 'fields/date_picker/js/locales/bootstrap-datepicker.' . $field['config']['language'] . '.js', array('cf-frontend-fields'), null, true);
+		wp_enqueue_script( 'cf-frontend-date-picker-lang', CFCORE_URL . 'fields/date_picker/js/locales/bootstrap-datepicker.' . $field['config']['language'] . '.js', array('cf-field'), null, true);
 	}
 }
 
@@ -30,7 +43,7 @@ if( !empty( $field['config']['autoclose'] ) ){
 <?php echo $wrapper_before; ?>
 	<?php echo $field_label; ?>
 	<?php echo $field_before; ?>
-		<input <?php echo $field_placeholder; ?> type="text" data-provide="cfdatepicker" data-field="<?php echo $field_base_id; ?>" class="<?php echo $field_class; ?>  is-cfdatepicker" id="<?php echo $field_id; ?>" <?php echo $has_lang; ?> data-date-format="<?php echo $field['config']['format']; ?>" <?php echo $start_end_atts; ?> <?php echo $is_autoclose; ?> name="<?php echo $field_name; ?>" value="<?php echo $field_value; ?>" <?php echo $field_required; ?>>
+		<input <?php echo $field_placeholder; ?> type="text" data-provide="cfdatepicker" data-field="<?php echo $field_base_id; ?>" class="<?php echo $field_class; ?>  is-cfdatepicker" id="<?php echo $field_id; ?>" <?php echo $has_lang; ?> data-date-format="<?php echo $field['config']['format']; ?>" <?php echo $start_end_atts; ?> <?php echo $is_autoclose; ?> <?php echo $daysOfWeekDisabled; ?> name="<?php echo $field_name; ?>" value="<?php echo $field_value; ?>" <?php echo $field_required; ?>>
 		<?php echo $field_caption; ?>
 	<?php echo $field_after; ?>
 <?php echo $wrapper_after; ?>
--- fields/date_picker/js/locales/bootstrap-datepicker.fr.js
+++ fields/date_picker/js/locales/bootstrap-datepicker.fr.js
@@ -1,17 +1,15 @@
 /**
  * French translation for bootstrap-datepicker
- * Nico Mollet <nico.mollet@gmail.com>
+ * Philippe Boissonneault <philippe.boissonneault@usherbrooke.ca>
  */
 ;(function($){
 	$.fn.cfdatepicker.dates['fr'] = {
-		days: ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"],
-		daysShort: ["dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam.", "dim."],
-		daysMin: ["d", "l", "ma", "me", "j", "v", "s", "d"],
-		months: ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"],
-		monthsShort: ["janv.", "févr.", "mars", "avril", "mai", "juin", "juil.", "août", "sept.", "oct.", "nov.", "déc."],
-		today: "Aujourd'hui",
-		clear: "Effacer",
-		weekStart: 1,
-		format: "dd/mm/yyyy"
+		days: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"],
+				daysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"],
+				daysMin: ["D", "L", "Ma", "Me", "J", "V", "S", "D"],
+				months: ["Janvier", "Férvier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+				monthsShort: ["Jan", "Fév", "Mar", "Avr", "Mai", "Jui", "Jul", "Aoû", "Sep", "Oct", "Nov", "Déc"],
+				today: "Aujourd'hui",
+				clear: "Effacer"
 	};
 }(jQuery));
